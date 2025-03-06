f8b93a83ee4832577c397a7489541d65e11ed843
Shubham Agrawal <sa280337@gmail.com>
2025-03-06 14:53:07 -0500

    working on user login and signup using passport

---
+++ routes/user.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const router = express.Router();
+const User = require("../models/user");
+const passport = require("passport");
+const wrapAsync = require("../utils/wrapAsync");
+
+router.get("/signup", (req, res) => {
+  res.render("users/signup");
+});
+
+router.post(
+  "/signup",
+  wrapAsync(async (req, res) => {
+    try {
+      let { username, email, password } = req.body;
+      const newUser = new User({ username, email });
+      const registeredUser = await User.register(newUser, password);
+      req.flash("success", "Welcome to Wanderlust!");
+      res.redirect("/listings");
+    } catch (e) {
+      req.flash("error", e.message);
+      res.redirect("/signup");
+    }
+  })
+);
+
+router.get("/login", (req, res) => {
+  res.render("users/login");
+});
+
+router.post(
+  "/login",
+  passport.authenticate("local", {
+    failureFlash: true,
+    failureRedirect: "/login",
+  }),
+  (req, res) => {
+    req.flash("success", "Welcome back!");
+    res.redirect("/listings");
+  }
+);
+
+module.exports = router;
